fc88ab5a017699b8230ffc9394cdfee662ae7d75
Andrey Ivanov <SuperJugger88@gmail.com>
2024-07-29 00:15:11 +0300

    feature: polished domain layer

--- domain/repository/user_repository.go
+++ domain/repository/user_repository.go
@@ -3,9 +3,7 @@ package repository
 import "stonewall-api/domain/entity"
 
 type UserRepository interface {
-	RegisterUser(user *entity.User) (*entity.User, error)
-	LoginUser(user *entity.User) error
-	//ResetPassword(user *entity.User) error
-	//VerifyByEmail(user *entity.User) error
-	//VerifyByPhone(user *entity.User) error
+	Create(user *entity.User) (*entity.User, error)
+	Get(user *entity.User) error
+	Update(user *entity.User) (*entity.User, error)
 }
