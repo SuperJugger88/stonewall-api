7cf20ca3bd541e3af58a4bcfd0f39ac37c96310d
Alexandr <ermishkin@logema.org>
2024-08-05 16:52:35 +0300

    Добавил метод для добавления пользователя в бд

--- app/models/user.go
+++ app/models/user.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -10,6 +11,23 @@ type User struct {
 	ID       uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
 	Email    string    `gorm:"size:100;not null;unique" json:"email"`
 	Password string    `gorm:"size:100;not null;" json:"password"`
+}
+
+type UserDTO struct {
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+// HashPassword хеширует пароль
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// CheckPasswordHash проверяет, соответствует ли хеш пароля
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
 // func Hash(password string) ([]byte, error) {
