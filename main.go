76a75e2504b8c9499fee7ee1bba8dfecb71cd538
SuperJugger88 <68207376+SuperJugger88@users.noreply.github.com>
2024-09-24 13:52:19 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,5 +8,14 @@ import (
 func main() {
 	db := database.InitDatabaseConnection()
 
+	conn, _ := db.DB()
+
+	defer func(conn *sql.DB) {
+		err := conn.Close()
+		if err != nil {
+			return
+		}
+	}(conn)
+
 	routes.SetupRouter(db)
 }
